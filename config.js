af84b2af52534b3a389bcd41a7e91b22abc028df
Darsh Patel <darshkpatel@users.noreply.github.com>
2018-09-06 20:20:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-module.exports.start = new Date(2018, 6, 16);
-module.exports.end = new Date(2018, 10, 14);
+module.exports.start = new Date(2018, 6, 16); // Semester Start
+module.exports.end = new Date(2018, 10, 14); // Semester End
 module.exports.username = ''
 module.exports.password = ''
-module.exports.semID = 'VL2018191' //Fall Sem  
-module.exports.save_file = false
+module.exports.semID = 'VL2018191' // HTML ID of semester found from VTOP HTML Source VL2018191 is for FALL SEMESTER
+module.exports.save_file = false // Save timetable in HTML Format
